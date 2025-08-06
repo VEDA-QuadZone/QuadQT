8933474bf68565c9c5cc48989da51353313648a5
Jonghwa Park <janjonghwa@gmail.com>
2025-08-06 10:33:23 +0900

    feat: final

--- QuadQT/copy_files.bat
+++ QuadQT/copy_files.bat
@@ -1,35 +1,43 @@
 @echo off
-echo QuadQT 설정 파일 및 인증서 복사 스크립트
+echo QuadQT DLL copy script
 
-set BUILD_DIR=build\Desktop_Qt_6_9_1_MinGW_64_bit-Debug\debug
+:: 가능한 빌드 디렉토리들을 순서대로 확인
+set BUILD_DIRS=build\Desktop_Qt_6_9_1_MinGW_64_bit-Debug\debug build\Desktop_Qt_6_9_1_MinGW_64_bit-Release\release build\Desktop_Qt_6_9_1_MinGW_64_bit-Release2\release
 
-echo 빌드 디렉토리: %BUILD_DIR%
+set BUILD_DIR=
+for %%d in (%BUILD_DIRS%) do (
+    if exist "%%d" (
+        set BUILD_DIR=%%d
+        goto :found
+    )
+)
 
-if not exist "%BUILD_DIR%" (
-    echo 오류: 빌드 디렉토리가 존재하지 않습니다.
+:found
+if "%BUILD_DIR%"=="" (
+    echo Error: No build directory found.
+    echo Checked directories:
+    for %%d in (%BUILD_DIRS%) do echo   %%d
     pause
     exit /b 1
 )
 
-echo config.ini 복사 중...
-copy /Y config.ini "%BUILD_DIR%\"
+echo Build directory found: %BUILD_DIR%
 
-echo resources 디렉토리 생성 중...
-if not exist "%BUILD_DIR%\resources" mkdir "%BUILD_DIR%\resources"
-if not exist "%BUILD_DIR%\resources\certs" mkdir "%BUILD_DIR%\resources\certs"
+:: DLL 파일들이 이미 있는지 확인
+if exist "%BUILD_DIR%\libopencv_core455.dll" (
+    echo OpenCV DLL files already exist in build directory.
+    echo Skipping copy operation.
+) else (
+    echo Copying OpenCV DLL files...
+    if exist "dlls" (
+        copy /Y "dlls\*.dll" "%BUILD_DIR%\"
+        echo Copy completed!
+    ) else (
+        echo Warning: dlls directory not found.
+    )
+)
 
-echo 인증서 파일들 복사 중...
-if exist "resources\certs\ca.cert.pem" copy /Y "resources\certs\ca.cert.pem" "%BUILD_DIR%\resources\certs\"
-if exist "resources\certs\client.cert.pem" copy /Y "resources\certs\client.cert.pem" "%BUILD_DIR%\resources\certs\"
-if exist "resources\certs\client.key.pem" copy /Y "resources\certs\client.key.pem" "%BUILD_DIR%\resources\certs\"
-
-echo 루트 디렉토리에도 인증서 복사 중...
-if exist "ca.cert.pem" copy /Y "ca.cert.pem" "%BUILD_DIR%\"
-if exist "client.cert.pem" copy /Y "client.cert.pem" "%BUILD_DIR%\"
-if exist "client.key.pem" copy /Y "client.key.pem" "%BUILD_DIR%\"
-
-echo OpenCV DLL 파일들 복사 중...
-if exist "dlls" copy /Y "dlls\*.dll" "%BUILD_DIR%\"
-
-echo 복사 완료!
+echo.
+echo Build directory: %BUILD_DIR%
+echo Ready to run QuadQT.exe
 pause
